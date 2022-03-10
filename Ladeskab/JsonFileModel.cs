3c567cb5f8889746174ce1e71845f428f7b161c9
Anton <202008162@post.au.dk>
2022-03-10 13:49:48 +0100

    Added JsonFileModel

---
+++ Ladeskab/JsonFileModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Ladeskab
+{
+    public class JsonFileModel: IJsonFileModel
+    {
+        public uint Rfid { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
