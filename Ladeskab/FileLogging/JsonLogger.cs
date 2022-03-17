18605f143bb14e2ac86d41757b2df34f0f00f502
Anton <202008162@post.au.dk>
2022-03-17 13:03:54 +0100

    Fix xD

--- Ladeskab/FileLogging/JsonLogger.cs
+++ Ladeskab/FileLogging/JsonLogger.cs
@@ -21,14 +21,22 @@ namespace Ladeskab
             string json = JsonSerializer.Serialize(jsonInput);
             var path = Environment.CurrentDirectory;
             string fileTime = time.ToString(CultureInfo.CurrentCulture);
-            string[] splitStrings = path.Split(@"\LadeskabHandIn2");;
+            string[] splitStrings;
+            if (path.Contains("JenkinsData"))
+            {
+                splitStrings = path.Split(@"\Ladeskab01F22");
+            }
+            else
+            {
+                splitStrings = path.Split(@"\LadeskabHandIn2");
+            }
             string invalid = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
             foreach (char c in invalid)
             {
                  fileTime= fileTime.Replace(c.ToString(), "");
             }
             File.WriteAllText(splitStrings[0] + @"\LadeSkabHandIn2\Ladeskab\Logfolder\" + fileTime +" Locked", json);
-            var readFile = File.ReadAllText(splitStrings[0] + @"\LadeSkabHandIn2\Ladeskab\Logfolder\" + fileTime+ " Locked");
+            var readFile = File.ReadAllText(splitStrings[0] + @"\LadeskabHandIn2\Ladeskab\Logfolder\" + fileTime+ " Locked");
             IJsonFileModel model = JsonSerializer.Deserialize<JsonFileModel>(readFile);
             return model;
         }
@@ -44,14 +52,22 @@ namespace Ladeskab
             string json = JsonSerializer.Serialize(jsonInput);
             var path = Environment.CurrentDirectory;
             string fileTime = time.ToLongTimeString();
-            string[] splitStrings = path.Split(@"\LadeskabHandIn2"); ;
+            string[] splitStrings;
+            if (path.Contains("JenkinsData"))
+            {
+                splitStrings = path.Split(@"\Ladeskab01F22");
+            }
+            else
+            {
+                splitStrings = path.Split(@"\LadeskabHandIn2");
+            }
             string invalid = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
             foreach (char c in invalid)
             {
                 fileTime = fileTime.Replace(c.ToString(), "");
             }
-            File.WriteAllText(splitStrings[0] + @"\LadeSkabHandIn2\Ladeskab\Logfolder\" + fileTime+ " Unlocked", json);
-            var readFile = File.ReadAllText(splitStrings[0] + @"\LadeSkabHandIn2\Ladeskab\Logfolder\" + fileTime + " Unlocked");
+            File.WriteAllText(splitStrings[0] + @"\LadeskabHandIn2\Ladeskab\Logfolder\" + fileTime+ " Unlocked", json);
+            var readFile = File.ReadAllText(splitStrings[0] + @"\LadeskabHandIn2\Ladeskab\Logfolder\" + fileTime + " Unlocked");
             IJsonFileModel model = JsonSerializer.Deserialize<JsonFileModel>(readFile);
             return model;
         }
